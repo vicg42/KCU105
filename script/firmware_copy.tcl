f85c539d7b045bb33d369857a198d087a931de74
vicg42 <vicg42@gmail.com>
2015-07-31 13:20:54 +0300

    script/

---
+++ script/firmware_copy.tcl
@@ -0,0 +1,2 @@
+file copy -force ../vv/prj/kcu105.runs/kcu105_impl_1/kcu105_main.bit ../firmware/
+file copy -force ../vv/prj/kcu105.runs/kcu105_impl_1/debug_nets.ltx ../vv/
---
+++ script/prom_download.bat
@@ -0,0 +1,5 @@
+call C:\\Xilinx\\Vivado\\2015.1\\.\\bin\\vivado.bat -mode batch -source prom_download.tcl
+if exist *isWriteableTest*.tmp del /F *isWriteableTest*.tmp
+if exist vivado_*.backup.jou del /F vivado_*.backup.jou
+if exist vivado_*.backup.log del /F vivado_*.backup.log
+pause
