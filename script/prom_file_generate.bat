9bcd1c5439af2dcc19d248b34320b99e3d19c834
vicg42 <vicg42@gmail.com>
2015-08-03 12:51:14 +0300

    script/prom_file_generate.bat

--- script/prom_file_generate.bat
+++ script/prom_file_generate.bat
@@ -1,5 +1,4 @@
-call C:\\Xilinx\\Vivado\\2015.1\\.\\bin\\vivado.bat -mode batch -source make_spi_mcs.tcl
+call C:\\Xilinx\\Vivado\\2015.1\\.\\bin\\vivado.bat -mode batch -source prom_file_generate.tcl
 if exist *isWriteableTest*.tmp del /F *isWriteableTest*.tmp
 if exist vivado_*.backup.jou del /F vivado_*.backup.jou
 if exist vivado_*.backup.log del /F vivado_*.backup.log
-pause
