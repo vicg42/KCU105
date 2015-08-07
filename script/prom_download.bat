15b5947d06ce1c7bef149ef13328a5d975db05c8
vicg42 <vicg42@gmail.com>
2015-08-07 11:51:17 +0300

    script/prom_download.bat --add delete log files

--- script/prom_download.bat
+++ script/prom_download.bat
@@ -2,3 +2,6 @@ call C:\\Xilinx\\Vivado\\2015.1\\.\\bin\\vivado.bat -mode batch -source prom_dow
 if exist *isWriteableTest*.tmp del /F *isWriteableTest*.tmp
 if exist vivado_*.backup.jou del /F vivado_*.backup.jou
 if exist vivado_*.backup.log del /F vivado_*.backup.log
+if exist webtalk_*.backup.jou del /F webtalk_*.backup.jou
+if exist webtalk_*.backup.log del /F webtalk_*.backup.log
+
