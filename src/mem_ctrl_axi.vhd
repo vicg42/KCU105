59418175409a7e08cee0249556e45a36dc5fb810
vicg42 <vicg42@gmail.com>
2015-08-07 12:45:57 +0300

    src/mem_ctrl_axi.vhd --correction for DDR4

--- src/mem_ctrl_axi.vhd
+++ src/mem_ctrl_axi.vhd
@@ -68,94 +68,82 @@ p_out_mem(i).clk <= i_clk(i);
 p_out_mem(i).rstn <= i_aresetn(i);
 
 m_mem_core : mem_ctrl_core_axi
-generic map (
-C_S_AXI_ID_WIDTH => C_AXIM_IDWIDTH,
-C_S_AXI_ADDR_WIDTH => C_AXI_AWIDTH,
-C_S_AXI_DATA_WIDTH => C_AXIM_DWIDTH
-)
 port map(
 -- AXI Slave Interface:
 -- Write Address Ports
-s_axi_awid     => p_in_mem (i).axiw.aid(C_AXIM_IDWIDTH - 1 downto 0),
-s_axi_awaddr   => p_in_mem (i).axiw.adr(C_AXI_AWIDTH - 1 downto 0)  ,
-s_axi_awlen    => p_in_mem (i).axiw.trnlen                        ,
-s_axi_awsize   => p_in_mem (i).axiw.dbus                          ,
-s_axi_awburst  => p_in_mem (i).axiw.burst                         ,
-s_axi_awlock   => p_in_mem (i).axiw.lock                          ,
-s_axi_awcache  => p_in_mem (i).axiw.cache                         ,
-s_axi_awprot   => p_in_mem (i).axiw.prot                          ,
-s_axi_awqos    => p_in_mem (i).axiw.qos                           ,
-s_axi_awvalid  => p_in_mem (i).axiw.avalid                        ,
-s_axi_awready  => p_out_mem(i).axiw.aready                        ,
+c0_ddr4_s_axi_awid    => p_in_mem (i).axiw.aid(C_AXIM_IDWIDTH - 1 downto 0),--: in STD_LOGIC_VECTOR ( 3 downto 0 );
+c0_ddr4_s_axi_awaddr  => p_in_mem (i).axiw.adr(C_AXI_AWIDTH - 1 downto 0)  ,--: in STD_LOGIC_VECTOR ( 30 downto 0 );
+c0_ddr4_s_axi_awlen   => p_in_mem (i).axiw.trnlen                          ,--: in STD_LOGIC_VECTOR ( 7 downto 0 );
+c0_ddr4_s_axi_awsize  => p_in_mem (i).axiw.dbus                            ,--: in STD_LOGIC_VECTOR ( 2 downto 0 );
+c0_ddr4_s_axi_awburst => p_in_mem (i).axiw.burst                           ,--: in STD_LOGIC_VECTOR ( 1 downto 0 );
+c0_ddr4_s_axi_awlock  => p_in_mem (i).axiw.lock                            ,--: in STD_LOGIC_VECTOR ( 0 to 0 );
+c0_ddr4_s_axi_awcache => p_in_mem (i).axiw.cache                           ,--: in STD_LOGIC_VECTOR ( 3 downto 0 );
+c0_ddr4_s_axi_awprot  => p_in_mem (i).axiw.prot                            ,--: in STD_LOGIC_VECTOR ( 2 downto 0 );
+c0_ddr4_s_axi_awqos   => p_in_mem (i).axiw.qos                             ,--: in STD_LOGIC_VECTOR ( 3 downto 0 );
+c0_ddr4_s_axi_awvalid => p_in_mem (i).axiw.avalid                          ,--: in STD_LOGIC;
+c0_ddr4_s_axi_awready => p_out_mem(i).axiw.aready                          ,--: out STD_LOGIC;
 -- Write Data Ports
-s_axi_wdata    => p_in_mem (i).axiw.data(C_AXIM_DWIDTH - 1 downto 0) ,
-s_axi_wstrb    => p_in_mem (i).axiw.dbe(C_AXIM_DWIDTH/8 - 1 downto 0),
-s_axi_wlast    => p_in_mem (i).axiw.dlast                         ,
-s_axi_wvalid   => p_in_mem (i).axiw.dvalid                        ,
-s_axi_wready   => p_out_mem(i).axiw.wready                        ,
+c0_ddr4_s_axi_wdata   => p_in_mem (i).axiw.data(C_AXIM_DWIDTH - 1 downto 0) ,--: in STD_LOGIC_VECTOR ( 511 downto 0 );
+c0_ddr4_s_axi_wstrb   => p_in_mem (i).axiw.dbe(C_AXIM_DWIDTH/8 - 1 downto 0),--: in STD_LOGIC_VECTOR ( 63 downto 0 );
+c0_ddr4_s_axi_wlast   => p_in_mem (i).axiw.dlast                            ,--: in STD_LOGIC;
+c0_ddr4_s_axi_wvalid  => p_in_mem (i).axiw.dvalid                           ,--: in STD_LOGIC;
+c0_ddr4_s_axi_wready  => p_out_mem(i).axiw.wready                           ,--: out STD_LOGIC;
 -- Write Response Ports
-s_axi_bid      => i_saxi_bid(i)(C_AXIM_IDWIDTH - 1 downto 0)        ,
-s_axi_bresp    => p_out_mem(i).axiw.resp                          ,
-s_axi_bvalid   => p_out_mem(i).axiw.rvalid                        ,
-s_axi_bready   => p_in_mem (i).axiw.rready                        ,
+c0_ddr4_s_axi_bid     => i_saxi_bid(i)(C_AXIM_IDWIDTH - 1 downto 0)      ,--: out STD_LOGIC_VECTOR ( 3 downto 0 );
+c0_ddr4_s_axi_bresp   => p_out_mem(i).axiw.resp                          ,--: out STD_LOGIC_VECTOR ( 1 downto 0 );
+c0_ddr4_s_axi_bvalid  => p_out_mem(i).axiw.rvalid                        ,--: out STD_LOGIC;
+c0_ddr4_s_axi_bready  => p_in_mem (i).axiw.rready                        ,--: in STD_LOGIC;
 -- Read Address Ports
-s_axi_arid     => p_in_mem (i).axir.aid(C_AXIM_IDWIDTH - 1 downto 0),
-s_axi_araddr   => p_in_mem (i).axir.adr(C_AXI_AWIDTH - 1 downto 0)  ,
-s_axi_arlen    => p_in_mem (i).axir.trnlen                        ,
-s_axi_arsize   => p_in_mem (i).axir.dbus                          ,
-s_axi_arburst  => p_in_mem (i).axir.burst                         ,
-s_axi_arlock   => p_in_mem (i).axir.lock                          ,
-s_axi_arcache  => p_in_mem (i).axir.cache                         ,
-s_axi_arprot   => p_in_mem (i).axir.prot                          ,
-s_axi_arqos    => p_in_mem (i).axir.qos                           ,
-s_axi_arvalid  => p_in_mem (i).axir.avalid                        ,
-s_axi_arready  => p_out_mem(i).axir.aready                        ,
+c0_ddr4_s_axi_arid    => p_in_mem (i).axir.aid(C_AXIM_IDWIDTH - 1 downto 0),--: in STD_LOGIC_VECTOR ( 3 downto 0 );
+c0_ddr4_s_axi_araddr  => p_in_mem (i).axir.adr(C_AXI_AWIDTH - 1 downto 0)  ,--: in STD_LOGIC_VECTOR ( 30 downto 0 );
+c0_ddr4_s_axi_arlen   => p_in_mem (i).axir.trnlen                          ,--: in STD_LOGIC_VECTOR ( 7 downto 0 );
+c0_ddr4_s_axi_arsize  => p_in_mem (i).axir.dbus                            ,--: in STD_LOGIC_VECTOR ( 2 downto 0 );
+c0_ddr4_s_axi_arburst => p_in_mem (i).axir.burst                           ,--: in STD_LOGIC_VECTOR ( 1 downto 0 );
+c0_ddr4_s_axi_arlock  => p_in_mem (i).axir.lock                            ,--: in STD_LOGIC_VECTOR ( 0 to 0 );
+c0_ddr4_s_axi_arcache => p_in_mem (i).axir.cache                           ,--: in STD_LOGIC_VECTOR ( 3 downto 0 );
+c0_ddr4_s_axi_arprot  => p_in_mem (i).axir.prot                            ,--: in STD_LOGIC_VECTOR ( 2 downto 0 );
+c0_ddr4_s_axi_arqos   => p_in_mem (i).axir.qos                             ,--: in STD_LOGIC_VECTOR ( 3 downto 0 );
+c0_ddr4_s_axi_arvalid => p_in_mem (i).axir.avalid                          ,--: in STD_LOGIC;
+c0_ddr4_s_axi_arready => p_out_mem(i).axir.aready                          ,--: out STD_LOGIC;
 -- Read Data Ports
-s_axi_rid      => i_saxi_rid(i)(C_AXIM_IDWIDTH - 1 downto 0)        ,
-s_axi_rdata    => p_out_mem(i).axir.data(C_AXIM_DWIDTH - 1 downto 0) ,
-s_axi_rresp    => p_out_mem(i).axir.resp                          ,
-s_axi_rlast    => p_out_mem(i).axir.dlast                         ,
-s_axi_rvalid   => p_out_mem(i).axir.dvalid                        ,
-s_axi_rready   => p_in_mem (i).axir.rready                        ,
+c0_ddr4_s_axi_rid     => i_saxi_rid(i)(C_AXIM_IDWIDTH - 1 downto 0)        ,--: out STD_LOGIC_VECTOR ( 3 downto 0 );
+c0_ddr4_s_axi_rdata   => p_out_mem(i).axir.data(C_AXIM_DWIDTH - 1 downto 0),--: out STD_LOGIC_VECTOR ( 511 downto 0 );
+c0_ddr4_s_axi_rresp   => p_out_mem(i).axir.resp                            ,--: out STD_LOGIC_VECTOR ( 1 downto 0 );
+c0_ddr4_s_axi_rlast   => p_out_mem(i).axir.dlast                           ,--: out STD_LOGIC;
+c0_ddr4_s_axi_rvalid  => p_out_mem(i).axir.dvalid                          ,--: out STD_LOGIC
+c0_ddr4_s_axi_rready  => p_in_mem (i).axir.rready                          ,--: in STD_LOGIC;
 
--- DDR3 Physical Interface
-ddr3_dq         => p_inout_phymem(i).dq     ,
-ddr3_addr       => p_out_phymem  (i).addr   ,
-ddr3_ba         => p_out_phymem  (i).ba     ,
-ddr3_ras_n      => p_out_phymem  (i).ras_n  ,
-ddr3_cas_n      => p_out_phymem  (i).cas_n  ,
-ddr3_we_n       => p_out_phymem  (i).we_n   ,
-ddr3_reset_n    => p_out_phymem  (i).reset_n,
-ddr3_cs_n       => p_out_phymem  (i).cs_n   ,
-ddr3_odt        => p_out_phymem  (i).odt    ,
-ddr3_cke        => p_out_phymem  (i).cke    ,
-ddr3_dm         => p_out_phymem  (i).dm     ,
-ddr3_dqs_p      => p_inout_phymem(i).dqs_p  ,
-ddr3_dqs_n      => p_inout_phymem(i).dqs_n  ,
-ddr3_ck_p       => p_out_phymem  (i).ck_p   ,
-ddr3_ck_n       => p_out_phymem  (i).ck_n   ,
+-- DDR4 Physical Interface
+c0_ddr4_act_n    => p_out_phymem  (i).act_n   ,--: out STD_LOGIC;
+c0_ddr4_adr      => p_out_phymem  (i).addr    ,--: out STD_LOGIC_VECTOR ( 16 downto 0 );
+c0_ddr4_ba       => p_out_phymem  (i).ba      ,--: out STD_LOGIC_VECTOR ( 1 downto 0 );
+c0_ddr4_bg       => p_out_phymem  (i).bg      ,--: out STD_LOGIC_VECTOR ( 0 to 0 );
+c0_ddr4_cke      => p_out_phymem  (i).cke     ,--: out STD_LOGIC_VECTOR ( 0 to 0 );
+c0_ddr4_odt      => p_out_phymem  (i).odt     ,--: out STD_LOGIC_VECTOR ( 0 to 0 );
+c0_ddr4_cs_n     => p_out_phymem  (i).cs_n    ,--: out STD_LOGIC_VECTOR ( 0 to 0 );
+c0_ddr4_ck_t     => p_out_phymem  (i).ck_t    ,--: out STD_LOGIC_VECTOR ( 0 to 0 );
+c0_ddr4_ck_c     => p_out_phymem  (i).ck_c    ,--: out STD_LOGIC_VECTOR ( 0 to 0 );
+c0_ddr4_reset_n  => p_out_phymem  (i).reset_n ,--: out STD_LOGIC;
+c0_ddr4_dm_dbi_n => p_out_phymem  (i).dm_dbi_n,--: inout STD_LOGIC_VECTOR ( 7 downto 0 );
+c0_ddr4_dq       => p_inout_phymem(i).dq      ,--: inout STD_LOGIC_VECTOR ( 63 downto 0 );
+c0_ddr4_dqs_c    => p_inout_phymem(i).dqs_c   ,--: inout STD_LOGIC_VECTOR ( 7 downto 0 );
+c0_ddr4_dqs_t    => p_inout_phymem(i).dqs_t   ,--: inout STD_LOGIC_VECTOR ( 7 downto 0 );
 
 --Status
-init_calib_complete => p_out_status.rdy(i),
-mmcm_locked     => i_mmcm_locked(i),
+c0_init_calib_complete => p_out_status.rdy(i),--: out STD_LOGIC;
 
-aresetn         => i_aresetn(i)           ,--input
-ui_clk_sync_rst => i_rst(i)               ,--output
-ui_clk          => i_clk(i)               ,--output
+
+c0_ddr4_aresetn         => i_aresetn(i)      ,--: in STD_LOGIC;
+c0_ddr4_ui_clk_sync_rst => i_rst(i)          ,--: out STD_LOGIC;
+c0_ddr4_ui_clk          => i_clk(i)          ,--: out STD_LOGIC;
+addn_ui_clkout1         => open              ,--: out STD_LOGIC;
+addn_ui_clkout2         => open              ,--: out STD_LOGIC;
+dbg_clk                 => open              ,--: out STD_LOGIC;
 
 --System
-sys_clk_i       => p_in_sys.clk    ,
-clk_ref_i       => p_in_sys.ref_clk,
-sys_rst         => p_in_sys.rst,
-
-device_temp_i   => (others=>'0'),
-
-app_sr_req      => '0',
-app_sr_active   => open,
-app_ref_req     => '0',
-app_ref_ack     => open,
-app_zq_req      => '0',
-app_zq_ack      => open
+c0_sys_clk_p => p_in_sys.clk_p ,--: in STD_LOGIC;
+c0_sys_clk_n => p_in_sys.clk_n ,--: in STD_LOGIC;
+sys_rst      => p_in_sys.rst    --: in STD_LOGIC
 );
 
 process(i_clk(i))
