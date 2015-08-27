1e5fc69c174ba4b175d46f5b9ee07fc70cfa9f64
vicg42 <vicg42@gmail.com>
2015-08-27 16:47:58 +0300

    script/prom_download.tcl

--- script/prom_download.tcl
+++ script/prom_download.tcl
@@ -1,5 +1,5 @@
-write_cfgmem -force -format MCS -size 32 -interface SPIx8 \
-             -loadbit "up 0x00000000 ../firmware/kcu105_main.bit" ../firmware/kcu105_main.mcs
+write_cfgmem -force -format mcs -size 256 -interface spix8 -verbose \
+             -loadbit "up 0x0 ../firmware/kcu105_main.bit"  ../firmware/kcu105_main.mcs
 
 open_hw
 connect_hw_server -url localhost:3121
@@ -10,7 +10,7 @@ open_hw_target
 current_hw_device [lindex [get_hw_devices] 0]
 refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] 0]
 
-create_hw_cfgmem -hw_device [lindex [get_hw_devices] 0] -mem_dev  [lindex [get_cfgmem_parts {n25q256-1.8v-spi-x1_x2_x4_x8}] 0]
+create_hw_cfgmem -hw_device [lindex [get_hw_devices] 0] -mem_device [lindex [get_cfgmem_parts {n25q256-1.8v-spi-x1_x2_x4_x8}] 0]
 set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
