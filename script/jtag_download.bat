3ff2a2d68c82200070a78e772c35b89f9458d908
vicg42 <vicg42@gmail.com>
2015-12-03 13:58:13 +0300

    script/*.bat --add enviroment param call %XILINX_VV%\bin\vivado.bat

--- script/jtag_download.bat
+++ script/jtag_download.bat
@@ -1,4 +1,4 @@
-call C:\\Xilinx\\Vivado\\2015.2\\.\\bin\\vivado.bat -mode batch -source jtag_download.tcl
+call %XILINX_VV%\bin\vivado.bat -mode batch -source jtag_download.tcl
 if exist *isWriteableTest*.tmp del /F *isWriteableTest*.tmp
 if exist vivado_*.backup.jou del /F vivado_*.backup.jou
 if exist vivado_*.backup.log del /F vivado_*.backup.log
