aaaf00c48288d7adc87cab6ec5215e4ad8704b63
vicg42 <vicg42@gmail.com>
2015-12-03 14:03:12 +0300

    script/*.bat --add enviroment param call %XILINX_VV%\bin\vivado.bat

--- script/prom_file_generate.bat
+++ script/prom_file_generate.bat
@@ -1,4 +1,4 @@
-call C:\\Xilinx\\Vivado\\2015.3\\.\\bin\\vivado.bat -mode batch -source prom_file_generate.tcl
+call %XILINX_VV%\bin\vivado.bat -mode batch -source prom_file_generate.tcl
 if exist *isWriteableTest*.tmp del /F *isWriteableTest*.tmp
 if exist vivado_*.backup.jou del /F vivado_*.backup.jou
 if exist vivado_*.backup.log del /F vivado_*.backup.log
