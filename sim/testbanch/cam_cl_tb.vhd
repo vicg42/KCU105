0edb9e08ef66610c5bc5f534a709af5a18c4dc33
vicg42 <vicg42@gmail.com>
2016-01-06 12:22:07 +0300

    sim/testbanch/cam_cl_tb.vhd

---
+++ sim/testbanch/cam_cl_tb.vhd
@@ -0,0 +1,208 @@
+-------------------------------------------------------------------------
+-- Engineer    : Golovachenko Victor
+--
+-- Create Date : 22.07.2012 11:10:51
+-- Module Name : cam_cl_tb
+--
+-------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.reduce_pack.all;
+use work.cl_pkg.all;
+use work.cam_cl_pkg.all;
+
+
+entity cam_cl_tb is
+generic(
+G_VCH_NUM : natural := 0;
+G_PKT_TYPE : natural := 1;
+G_PKT_HEADER_SIZE : natural := 16; --Header Byte Count
+G_PKT_CHUNK_SIZE : natural := 1024; --Data Chunk
+G_CL_PIXBIT : natural := 8; --Amount bit per 1 pix
+G_CL_TAP : natural := 8; --Amount pixel per 1 clk
+G_CL_CHCOUNT : natural := 3
+);
+port(
+--------------------------------------------------
+--CameraLink Interface
+--------------------------------------------------
+p_in_tfg_n : in  std_logic; --Camera -> FG
+p_in_tfg_p : in  std_logic;
+p_out_tc_n : out std_logic; --Camera <- FG
+p_out_tc_p : out std_logic;
+
+--X,Y,Z : 0,1,2
+p_in_cl_clk_p : in  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_in_cl_clk_n : in  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_in_cl_di_p  : in  std_logic_vector((4 * G_CL_CHCOUNT) - 1 downto 0);
+p_in_cl_di_n  : in  std_logic_vector((4 * G_CL_CHCOUNT) - 1 downto 0);
+
+--------------------------------------------------
+--VideoPkt Output
+--------------------------------------------------
+p_out_bufpkt_d     : out  std_logic_vector(63 downto 0);
+p_in_bufpkt_rd     : in   std_logic;
+p_in_bufpkt_rdclk  : in   std_logic;
+p_out_bufpkt_empty : out  std_logic;
+
+--------------------------------------------------
+--
+--------------------------------------------------
+p_out_status   : out  std_logic_vector(C_CAM_STATUS_LASTBIT downto 0)
+);
+end entity cam_cl_tb;
+
+architecture behavior of cam_cl_tb is
+
+constant CI_BUFPKT_RDCLK_PERIOD : TIME := 6.4 ns; --156.25MHz
+
+
+component cam_cl_main is
+generic(
+G_VCH_NUM : natural := 0;
+G_PKT_TYPE : natural := 1;
+G_PKT_HEADER_SIZE : natural := 16; --Header Byte Count
+G_PKT_CHUNK_SIZE : natural := 1024; --Data Chunk
+G_CL_PIXBIT : natural := 1; --Amount bit per 1 pix
+G_CL_TAP : natural := 8; --Amount pixel per 1 clk
+G_CL_CHCOUNT : natural := 1
+);
+port(
+--------------------------------------------------
+--
+--------------------------------------------------
+p_in_cam_ctrl_rx  : in  std_logic;
+p_out_cam_ctrl_tx : out std_logic;
+p_in_time         : in  std_logic_vector(31 downto 0);
+
+--------------------------------------------------
+--CameraLink Interface
+--------------------------------------------------
+p_in_tfg_n : in  std_logic; --Camera -> FG
+p_in_tfg_p : in  std_logic;
+p_out_tc_n : out std_logic; --Camera <- FG
+p_out_tc_p : out std_logic;
+
+--X,Y,Z : 0,1,2
+p_in_cl_clk_p : in  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_in_cl_clk_n : in  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_in_cl_di_p  : in  std_logic_vector((4 * G_CL_CHCOUNT) - 1 downto 0);
+p_in_cl_di_n  : in  std_logic_vector((4 * G_CL_CHCOUNT) - 1 downto 0);
+
+--------------------------------------------------
+--VideoPkt Output
+--------------------------------------------------
+p_out_bufpkt_d     : out  std_logic_vector(63 downto 0);
+p_in_bufpkt_rd     : in   std_logic;
+p_in_bufpkt_rdclk  : in   std_logic;
+p_out_bufpkt_empty : out  std_logic;
+
+--------------------------------------------------
+--
+--------------------------------------------------
+p_out_status   : out  std_logic_vector(C_CAM_STATUS_LASTBIT downto 0);
+
+--------------------------------------------------
+--DBG
+--------------------------------------------------
+p_out_tst : out  std_logic_vector(1 downto 0);
+p_in_tst  : in   std_logic_vector(0 downto 0);
+
+--p_in_refclk : in std_logic;
+--p_in_clk : in std_logic;
+p_in_rst : in std_logic
+);
+end component cam_cl_main;
+
+signal p_in_rst           : std_logic;
+signal p_in_clk           : std_logic;
+signal g_host_clk         : std_logic;
+
+signal i_bufpkt_rd        : std_logic;
+signal i_bufpkt_rdclk     : std_logic;
+signal i_bufpkt_empty     : std_logic;
+
+
+
+begin --architecture behavior of cam_cl_tb is
+
+
+gen_clk1 : process
+begin
+i_bufpkt_rdclk <= '0';
+wait for (CI_BUFPKT_RDCLK_PERIOD / 2);
+i_bufpkt_rdclk <= '1';
+wait for (CI_BUFPKT_RDCLK_PERIOD / 2);
+end process;
+
+
+p_in_rst <= '1','0' after 1 us;
+
+
+
+--***********************************************************
+--
+--***********************************************************
+m_cam : cam_cl_main
+generic map(
+G_VCH_NUM => G_VCH_NUM,
+G_PKT_TYPE => G_PKT_TYPE,
+G_PKT_HEADER_SIZE => G_PKT_HEADER_SIZE, --Header Byte Count
+G_PKT_CHUNK_SIZE => G_PKT_CHUNK_SIZE, --Data Chunk
+G_CL_PIXBIT => G_CL_PIXBIT, --Amount bit per 1 pix
+G_CL_TAP => G_CL_TAP, --Amount pixel per 1 clk
+G_CL_CHCOUNT => G_CL_CHCOUNT
+)
+port map(
+--------------------------------------------------
+--
+--------------------------------------------------
+p_in_cam_ctrl_rx  => '1',
+p_out_cam_ctrl_tx => open,
+p_in_time         => (others => '0'),
+
+--------------------------------------------------
+--CameraLink Interface
+--------------------------------------------------
+p_in_tfg_n => '0',
+p_in_tfg_p => '1',
+p_out_tc_n => open,
+p_out_tc_p => open,
+
+--X,Y,Z : 0,1,2
+p_in_cl_clk_p => p_in_cl_clk_p,
+p_in_cl_clk_n => p_in_cl_clk_n,
+p_in_cl_di_p  => p_in_cl_di_p ,
+p_in_cl_di_n  => p_in_cl_di_n ,
+
+--------------------------------------------------
+--VideoPkt Output
+--------------------------------------------------
+p_out_bufpkt_d     => p_out_bufpkt_d,
+p_in_bufpkt_rd     => i_bufpkt_rd   ,
+p_in_bufpkt_rdclk  => i_bufpkt_rdclk,
+p_out_bufpkt_empty => i_bufpkt_empty,
+
+--------------------------------------------------
+--
+--------------------------------------------------
+p_out_status   => p_out_status,
+
+--------------------------------------------------
+--DBG
+--------------------------------------------------
+p_out_tst => open,
+p_in_tst  => (others => '0'),
+
+--p_in_refclk : in std_logic;
+--p_in_clk : in std_logic;
+p_in_rst => p_in_rst
+);
+
+i_bufpkt_rd <= (not i_bufpkt_empty);
+
+
+end architecture behavior;
---
+++ sim/testbanch/cam_core_tb.vhd
@@ -0,0 +1,162 @@
+-------------------------------------------------------------------------
+-- Engineer    : Golovachenko Victor
+--
+-- Create Date : 22.07.2012 11:10:51
+-- Module Name : cl_main
+--
+-------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity cl_main is
+generic(
+G_CL_PIXBIT : natural := 8; --Amount bit per 1 pix
+G_CL_TAP : natural := 8; --Amount pixel per 1 clk
+G_CL_CHCOUNT : natural := 1
+);
+port(
+--------------------------------------------------
+--CameraLink
+--------------------------------------------------
+--X,Y,Z : 0,1,2
+p_in_cl_clk_p : in  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_in_cl_clk_n : in  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_in_cl_di_p  : in  std_logic_vector((4 * G_CL_CHCOUNT) - 1 downto 0);
+p_in_cl_di_n  : in  std_logic_vector((4 * G_CL_CHCOUNT) - 1 downto 0);
+
+--------------------------------------------------
+--VideoOut
+--------------------------------------------------
+p_out_plllock: out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_out_link   : out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_out_fval   : out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0); --frame valid
+p_out_lval   : out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0); --line valid
+p_out_dval   : out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0); --data valid
+p_out_rxbyte : out  std_logic_vector((G_CL_PIXBIT * G_CL_TAP) - 1 downto 0);
+p_out_rxclk  : out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+
+--------------------------------------------------
+--DBG
+--------------------------------------------------
+--p_out_tst : out  std_logic_vector(31 downto 0);
+--p_in_tst  : in   std_logic_vector(31 downto 0);
+
+--p_in_refclk : in std_logic;
+--p_in_clk : in std_logic;
+p_in_rst : in std_logic
+);
+end entity cl_main;
+
+architecture behavior of cl_main is
+
+constant CI_CLK_PERIOD : TIME := 11.765 ns; --85MHz
+
+type TFsm is (
+S_VSYNC,
+S_HSYNC,
+S_PIX
+);
+signal i_fsm     : TFsm;
+
+signal i_rst     : std_logic;
+signal i_clk     : std_logic;
+
+signal i_pixcnt  : unsigned(15 downto 0) := (others => '0');
+signal i_cnt     : unsigned(15 downto 0) := (others => '0');
+signal i_linecnt : unsigned(15 downto 0) := (others => '0');
+signal i_fval    : std_logic := '0';
+signal i_lval    : std_logic := '0';
+
+
+
+begin --architecture behavior of cl_main is
+
+
+gen_clk0 : process
+begin
+i_clk <= '0';
+wait for (CI_CLK_PERIOD / 2);
+i_clk <= '1';
+wait for (CI_CLK_PERIOD / 2);
+end process;
+
+
+i_rst <= '1','0' after 2.5 us;
+
+
+p_out_plllock <= ((not i_rst) & (not i_rst) & (not i_rst));--(others => '1');--: out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_out_link   <= ((not i_rst) & (not i_rst) & (not i_rst));--;--(others => '1');--: out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+p_out_fval   <= (i_fval & i_fval & i_fval);--: out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0); --frame valid
+p_out_lval   <= (i_lval & i_lval & i_lval);--: out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0); --line valid
+p_out_dval   <= (others => '1');--: out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0); --data valid
+p_out_rxbyte <= std_logic_vector(RESIZE(i_pixcnt, p_out_rxbyte'length));--: out  std_logic_vector((G_CL_PIXBIT * G_CL_TAP) - 1 downto 0);
+p_out_rxclk  <= (i_clk & i_clk & i_clk);--: out  std_logic_vector(G_CL_CHCOUNT - 1 downto 0);
+
+
+process(i_rst, i_clk)
+begin
+if rising_edge(i_clk) then
+if (i_rst = '1') then
+
+i_cnt <= (others => '0'); i_pixcnt <= (others => '0');
+i_linecnt <= (others => '0');
+i_fval <= '0';
+i_lval <= '0';
+
+else
+  case i_fsm is
+
+    when S_VSYNC =>
+
+      if (i_cnt = TO_UNSIGNED(48, i_cnt'length)) then
+        i_cnt <= (others => '0');
+        i_fval <= '1';
+        i_lval <= '0';
+        i_fsm <= S_HSYNC;
+      else
+        i_fval <= '0';
+        i_lval <= '0';
+        i_cnt <= i_cnt + 1;
+      end if;
+
+    when S_HSYNC =>
+
+      if (i_cnt = TO_UNSIGNED(32, i_cnt'length)) then
+        i_cnt <= (others => '0');
+        i_fval <= '1';
+        i_lval <= '1';
+        i_fsm <= S_PIX;
+      else
+        i_fval <= '1';
+        i_lval <= '0';
+        i_cnt <= i_cnt + 1;
+      end if;
+
+    when S_PIX =>
+
+      if (i_cnt = TO_UNSIGNED(63, i_cnt'length)) then
+        i_cnt <= (others => '0'); i_pixcnt <= (others => '0');
+        if (i_linecnt = TO_UNSIGNED(8, i_cnt'length)) then
+          i_linecnt <= (others => '0');
+          i_fval <= '0';
+          i_lval <= '0';
+          i_fsm <= S_VSYNC;
+        else
+          i_linecnt <= i_linecnt + 1;
+          i_fval <= '1';
+          i_lval <= '0';
+          i_fsm <= S_HSYNC;
+        end if;
+      else
+        i_cnt <= i_cnt + 1;
+        i_pixcnt <= i_pixcnt + 1;
+      end if;
+  end case;
+end if;
+end if;
+end process;
+
+
+
+end architecture behavior;
