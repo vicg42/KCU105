b6065fa3e1a266f6a44c41adea4272363623d5dc
vicg42 <vicg42@gmail.com>
2016-05-31 11:24:53 +0300

    linux/driver/test_spd

--- linux/driver/test_spd/test_spd.c
+++ linux/driver/test_spd/test_spd.c
@@ -1,5 +1,5 @@
 /*
- *  file:   drv_speed_tst.c
+ *  file:   test_spd.c
  *  date:   31.05.2016 11:12:43
  *  authors:
  */
@@ -9,25 +9,25 @@
 
 // local functions -------------------------------------------------------------
 
-static int __init test_init(void)
-static void __exit test_exit(void)
+static int __init test_spd_init(void)
+static void __exit test_spd_exit(void)
 
 // kernel routine (bind functions) ---------------------------------------------
 
-module_init(test_init);
-module_exit(test_exit);
+module_init(test_spd_init);
+module_exit(test_spd_exit);
 
 // local variables -------------------------------------------------------------
 
 
 // implementation --------------------------------------------------------------
 
-static int __init test_init(void)
+static int __init test_spd_init(void)
 {
  printk(KERN_ALERT "Hello, world\n");
  return 0;
 }
-static void __exit test_exit(void)
+static void __exit test_spd_exit(void)
 {
  printk(KERN_ALERT "Goodbye, cruel world\n");
 }
